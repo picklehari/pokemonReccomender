19345cf5d4fec473d06a6ea5e8e8ef3290aa5e9e
picklehari <harirajmanghat123@gmail.com>
2018-08-31 21:05:53 +0530

    Minor edits

--- contentBasedFiltering.py
+++ contentBasedFiltering.py
@@ -1,6 +1,7 @@
 import pandas
-import numpy
+#import numpy
 import ast
+import operator
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import linear_kernel
@@ -10,14 +11,19 @@ pokemon = pandas.read_csv('pokemon.csv')
 #Building a content based filtering system 
 #Properties used are type,Legendary,canMegaEvolve
 #Replacing boolean values with strings
+def simpleReccomendation(x):
+    return str((7*x['Attack']) + (5*x['Defense']) + (6*x['Speed'])-(9*x['Catch_Rate'])) #Weighted Popularity
+    
+
+
 pokemon['isLegendary']=pokemon['isLegendary'].replace(True,'Legendary')
 pokemon['isLegendary']=pokemon['isLegendary'].replace(False,'notLegendary')
 pokemon['hasMegaEvolution']=pokemon['hasMegaEvolution'].replace(True,'megaEvolve')
 pokemon['hasMegaEvolution']=pokemon['hasMegaEvolution'].replace(False,'notMegaEvolve')
 pokemon['Type_2']=pokemon['Type_2'].fillna('')
-
+pokemon['popularity'] = pokemon.apply(simpleReccomendation,axis=1)
 def createSoup(x):
-    return ''.join(x['Type_1']) + ' ' + ''.join(x['Type_2'])+ ' ' +''.join(x['isLegendary'])+' '+''.join(x['hasMegaEvolution'])
+    return (''.join(x['Type_1']) + ' ' + ''.join(x['Type_2'])+ ' ' +''.join(x['isLegendary'])+' '+''.join(x['hasMegaEvolution'])+' '+''.join(x['popularity']))
     
 pokemon['soup'] =pokemon.apply(createSoup,axis=1)
 
@@ -25,19 +31,20 @@ count = CountVectorizer(stop_words='english')
 countMatrix =count.fit_transform(pokemon['soup'])
 
 cosinSim = cosine_similarity(countMatrix,countMatrix)
-pokemon = pokemon.reset_index()
+#pokemon = pokemon.reset_index()
 indices = pandas.Series(pokemon.index,index=pokemon['Name']).drop_duplicates()
 
 def getRecommendations(Name,cosinSim=cosinSim):
     idx = indices[Name]
     simScores = list(enumerate(cosinSim[idx]))
-    simScores = sorted(simScores,key = lambda x: x[1],reverse=True)
-    simScores = simScores[0:11]
+
+    simScores = sorted(simScores.items(),key = operator.itemgetter(1),reverse=True)
+    simScores = simScores[1:11]
     pokemonIndices = list()
     for i in simScores:
-        if i[0] == idx:
-            continue
         pokemonIndices.append(i[0])
     return pokemon['Name'].iloc[pokemonIndices]
+    
+
 preferredPokemon = input('Enter a preferred pokemon (Sentence Case) ')
 print(getRecommendations(preferredPokemon,cosinSim))
--- simpleReccomendations.py
+++ simpleReccomendations.py
@@ -6,3 +6,4 @@ class Pokemon:
         pokemon['popularity'] = ((7*pokemon['Attack']) + (5*pokemon['Defense']) + (6*pokemon['Speed'])-(9*pokemon['Catch_Rate'])) #Weighted Popularity
         pokemon = pokemon.sort_values('popularity',ascending=False) #Sorting
         pokemon = pokemon.drop('popularity',axis=1) #Dropping popularity
+        
